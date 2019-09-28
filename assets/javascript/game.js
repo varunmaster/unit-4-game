7e530a10606fcaf6dc32ab1a2cf90ae794ebff11
Varun Master <varunmaster95@gmail.com>
2019-09-28 10:03:38 -0400

    adding some addtional jquery functions for resetting and reassigning gem values. TODO: find way to connect the values of the gem from js to html and retrieve values to add to userNumTotal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
 //Generating the list from 19 --> 120 (inclusive) and will not be changing this list again so const is used (same logic for genValList)
 const numList = [];
-for(var i = 19; i < 121; i++) { //doing < 121 bc we want 120 to be inclusive when we do math.random()
+for (var i = 19; i < 121; i++) { //doing < 121 bc we want 120 to be inclusive when we do math.random()
     numList.push(i);
 }
 
 const gemValList = [];
-for(var i = 1; i < 13; i++){ //doing < 13 bc we want 12 to be inclusive when we do math.random()
+for (var i = 1; i < 13; i++) { //doing < 13 bc we want 12 to be inclusive when we do math.random()
     gemValList.push(i);
 }
 
 var targetNum = getTargetNum();
-var gemArr = {
+
+var gemObj = {
     "emerald": setGemValues(),
     "diamond": setGemValues(),
     "sapphire": setGemValues(),
@@ -19,32 +20,36 @@ var gemArr = {
 var userNumTotal = 0;
 var gameComplete = isUserOver();
 var wins = 0;
-var loss  = 0;
+var loss = 0;
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(".gem").on("click", function() { //this refers to any of the "gem" class pressed in html
+    $(".gem").on("click", function () { //this refers to any of the "gem" class pressed in html
         userNumTotal += ($(this).val());
-        if(userNumTotal > targetNum) {
+        $(".targetNum").text(userNumTotal);
+        if (userNumTotal > targetNum) {
             loss++;
             newGame();
-        }else if (userNumTotal === targetNum) {
+        } else if (userNumTotal === targetNum) {
             wins++;
             newGame();
-        }else {
-            //continue; //error here in console
         }
-    }); 
+    });
 
 }); //Document.ready ends here
 
 
+function assginValuesToGems() {
+    $(".emerald").attr("value", gemObj.emerald);
+    $(".diamond").attr("value", gemObj.diamond);
+    $(".sapphire").attr("value", gemObj.sapphire);
+    $(".ruby").attr("value", gemObj.ruby);
+}
 
-
-function isUserOver(){
-    if(parseInt(userNumTotal) > targetNum){
+function isUserOver() {
+    if (parseInt(userNumTotal) > targetNum) {
         return true;
     }
 }
@@ -53,23 +58,24 @@ function getTargetNum() {
     return numList[Math.floor(Math.random() * numList.length)];
 }
 
-function setGemValues() { 
+function setGemValues() {
     return gemValList[Math.floor(Math.random() * gemValList.length)];
 }
 
-function newGame() { 
+function newGame() {
     targetNum = getTargetNum();
-    gemArr = {
+    userNumTotal = 0;
+    gemObj = {
         "emerald": setGemValues(),
         "diamond": setGemValues(),
         "sapphire": setGemValues(),
         "ruby": setGemValues()
     };
-
+    assginValuesToGems();
 }
 
 console.log("targetNum: ", targetNum);
-console.log("gem", gemArr.emerald);
-console.log("gem", gemArr.diamond);
-console.log("gem", gemArr.sapphire);
-console.log("gem", gemArr.ruby);
+console.log("gem", gemObj.emerald);
+console.log("gem", gemObj.diamond);
+console.log("gem", gemObj.sapphire);
+console.log("gem", gemObj.ruby);
